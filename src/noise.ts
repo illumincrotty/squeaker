49f43fe8cab58931eb2d70550773050336cff88e
Brian_Crotty <5673408+illumincrotty@users.noreply.github.com>
2021-08-19 00:17:22 -0500

    First attempt at noise functions

--- src/noise.ts
+++ src/noise.ts
@@ -1,68 +1,67 @@
-import { range } from './util';
-/* eslint-disable @typescript-eslint/no-magic-numbers */
+// /* eslint-disable @typescript-eslint/no-magic-numbers */
+type blendingFunction = (input: number) => number;
+
 /**
  * oldBlendingFunction.
+ * uses f(x) = -2x^3+3x^2
  *
- * @param input -
- * @returns
+ * @param input - number between 0 and 1
+ * @returns number between 0 and 1
  */
-const oldBlendingFunction = (input: number): number =>
+export const oldBlendingFunction: blendingFunction = (input: number): number =>
 	input ** 2 * (3 - 2 * input);
 
 /**
  * newBlendingFunction.
+ * uses f(x) = 6x^5-15x^4+10x^3
  *
- * @param input -
- * @returns
+ * @param input - number between 0 and 1
+ * @returns number between 0 and 1
  */
-const newBlendingFunction = (input: number): number =>
+export const newBlendingFunction: blendingFunction = (input: number): number =>
 	input ** 3 * (10 + input * (-15 + input * 6));
 
-const linearInterpolation = (
-	a: number,
-	b: number,
-	t: number
-): number => a * (1 - t) + b * t;
+/**
+ * 1d linearInterpolation.
+ *
+ * @param a - previous point
+ * @param b - next point
+ * @param t - the position in between
+ * @returns a number betwixt a and
+ */
+const _linearInterpolation = (a: number, b: number, t: number): number =>
+	a * (1 - t) + b * t;
 
+/**
+ * 1d perlinNoiseFactory.
+ *
+ * @param randomFunction - the desired random number generator function (defaults to Math.random)
+ * @param size - the size of the functions domain (numbers outside this domain will be modulus this number, NOT and error)
+ * @param blendingFunction - the function used to interpolate peaks of the domain
+ * @returns a perlin noise generator
+ */
 export const perlinNoiseFactory = (
 	randomFunction: () => number = Math.random,
-	blendingFunction: (input: number) => number = newBlendingFunction,
-	scale = 1
+	size = 256,
+	blendingFunction: blendingFunction = newBlendingFunction
 ): ((x: number) => number) => {
-	const _vertices = 256;
+	const _vertices = size;
 
 	const _randomValues = new Uint8Array(_vertices).map(
-		(_, _index) => randomFunction() * 256
+		(_, _index) => randomFunction() * _vertices
 	);
 
-	console.log(_randomValues);
+	return (input: number): number => {
+		const xFloor = Math.floor(input); // round toward -infinity
+		const xMin = xFloor % _vertices; // mod size
+		const xMax = Math.ceil(input) % _vertices; // round to +infinity mod size
 
-	return (x: number): number => {
-		const scaledX = x * scale;
-		const xFloor = Math.floor(scaledX);
-		const t = scaledX - xFloor;
-		const tRemapSmoothstep = blendingFunction(t);
-
-		const xMin = xFloor % _vertices;
-		const xMax = (xMin + 1) % _vertices;
-
-		const y = linearInterpolation(
+		const y = _linearInterpolation(
 			_randomValues[xMin],
 			_randomValues[xMax],
-			tRemapSmoothstep
+			blendingFunction(input - xFloor /* just the fractional portion */)
 		);
-
-		// console.log(
-		// 	`input: ${x} floor: ${xFloor} scaled: ${scaledX} xMin: ${xMin} xmax: ${xMax}`
-		// );
 
 		return y / _vertices;
 	};
 };
-
-const noise = perlinNoiseFactory();
-range(0, 10, 0.1).map((value) =>
-	console.log(`input: ${value.toFixed(2)}\t noise: ${noise(value)}`)
-);
-
-export { oldBlendingFunction, newBlendingFunction };
