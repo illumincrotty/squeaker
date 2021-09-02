cc9903a79ae1cc53eb6f0d596b0cee0ce0a06be6
Brian <5673408+illumincrotty@users.noreply.github.com>
2021-09-02 13:50:45 -0500

    Change type files to be d.ts and fix exports

--- src/squeaker.ts
+++ src/squeaker.ts
@@ -1,12 +1,18 @@
 export * from './perlin/perlinIndex';
 export * from './util';
 export * from './interpolation';
-export {
-	alea,
-	aleaFactory,
-	functionalMulberryFactory,
-	mulberryFactory,
-} from './random/randomIndex';
+export * from './random/randomIndex';
+export type {
+	interpolationFunction,
+	noiseFunction1d,
+	noiseFunction2d,
+	noiseFunction3d,
+	noiseFunction4d,
+	vector2d,
+	vector3d,
+	vectorArray2d,
+	vectorArray3d,
+} from './noiseTypes';
 // export default {
 // 	perlinNoise1dFactory,
 // 	perlinNoise2dFactory,
