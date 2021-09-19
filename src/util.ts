e40a774a461a74f66c9e79d6ca20b0a89b86b137
Brian <5673408+illumincrotty@users.noreply.github.com>
2021-09-19 18:20:57 -0500

    Remove unneeded range utilities

--- src/util.ts
+++ src/util.ts
@@ -8,94 +8,6 @@ import type {
 import { aleaFactory } from './random/alea';
 
 /**
- * Simple range utility, not very efficient
- *
- * @deprecated
- * @param start - starting number (inclusive)
- * @param end - ending number (exclusive)
- * @param step - step per iteration (can be negative)
- * @returns an array of the numbers from min to max
- */
-export const range = (start: number, end: number, step = 1): number[] => {
-	if ((end - start) / step <= 0) return [];
-	return [start, ...range(start + step, end, step)];
-};
-
-/**
- * rangeParameters
- *
- * the parameters for a range generating function
- */
-export interface rangeParameters {
-	/**
-	 * starting number (inclusive)
-	 */
-	start: number;
-	/**
-	 * ending number (exclusive)
-	 */
-	end: number;
-	/**
-	 * step per iteration (can be negative), defaults to 1
-	 */
-	step?: number;
-}
-
-/**
- * Simple range generator utility
- *
- * @param options - options for generation
- * @param options.start - starting number (inclusive)
- * @param options.end - ending number (exclusive)
- * @param options.step - step per iteration (can be negative)
- * @yields an array of the numbers from min to max
- */
-export function* rangeGenerator({
-	start,
-	end,
-	step = 1,
-}: rangeParameters): Generator<number, void, unknown> {
-	const stepsNeeded = (end - start - Number.EPSILON) / step;
-	for (
-		let place = start, currentStep = 0;
-		currentStep < stepsNeeded;
-		currentStep += 1, place += step
-	) {
-		yield place;
-	}
-	return;
-}
-
-/**
- * Memory Efficient Grid Generation
- *
- * @param xOptions - parameters for domain
- * @param yOptions  - parameters for range
- * @param mappingFunction - converts coordinates to desired value
- * @yields mapped value
- */
-export function* flatGridGenerator(
-	xOptions: rangeParameters,
-	yOptions: rangeParameters,
-	mappingFunction: (x: number, y: number) => number = (_0, _1): number => 0
-): Generator<number, void, void> {
-	const xGen = rangeGenerator(xOptions);
-	let yGen = rangeGenerator(yOptions);
-	let xtemp = xGen.next();
-	let ytemp = yGen.next();
-	while (!xtemp.done) {
-		while (!ytemp.done) {
-			yield mappingFunction(xtemp.value, ytemp.value);
-			ytemp = yGen.next();
-		}
-		yGen = rangeGenerator(yOptions);
-		ytemp = yGen.next();
-		xtemp = xGen.next();
-	}
-	return;
-}
-
-/**
  * Modulus that consistenly works on negative numbers
  *
  * @param divisor - the number to be divided
@@ -336,8 +248,7 @@ export const pair2d = (x: number, y: number): number => szudzikPair(x, y);
 /*@__PURE__*/
 /*@__INLINE__*/
 export const pair3d = (x: number, y: number, z: number): number =>
-	szudzikPair(szudzikPair(x, y) * 17, z);
-
+	((x * 251) ^ (y * 239) ^ (z * 241)) * 4103;
 /**
  * processOptions
  *
